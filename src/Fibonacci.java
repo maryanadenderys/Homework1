1a870a5737d1dbb5b076f884736a31e0c86a9ba6
maryanadenderys <maryanadenderys@gmail.com>
2015-02-25 22:32:23 +0200

    ya maryana

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -14,6 +14,8 @@ public class Fibonacci {
         int[] fibonaci = new int[n];
         int max_odd = fibonaci[0];
         int max_event = fibonaci[0];
+        double odd=0;
+        double event=0;
         fibonaci[0] = 0;
         fibonaci[1] = 1;
         System.out.print(fibonaci[0] + ", ");
@@ -22,16 +24,17 @@ public class Fibonacci {
         for(int i = 2; i < n; ++i) {
             fibonaci[i] = fibonaci[i - 1] + fibonaci[i - 2];
             System.out.print(fibonaci[i] + ", ");
-            if(fibonaci[i] % 2 != 0 && max_odd < fibonaci[i]) {
+            if(fibonaci[i] % 2 != 0 && max_odd < fibonaci[i]) {odd++;
                 max_odd = fibonaci[i];
             }
 
-            if(fibonaci[i] % 2 != 1 && max_event < fibonaci[i]) {
+            if(fibonaci[i] % 2 != 1 && max_event < fibonaci[i]) {event++;
                 max_event = fibonaci[i];
             }
         }
 
-        System.out.print("max_odd= " + max_odd + ", ");
-        System.out.print("max_event= " + max_event);
+
+        System.out.print("max_odd= " + max_odd + ", %odd= " + (odd/n)*100 + ", ");
+        System.out.print("max_event= " + max_event + ", %event= " + (event/n)*100);
     }
 }
